36a4788a435a95292232eb6bccc4056bdb147758
Saurbh Moynak <94280322+Saurbhmoynak@users.noreply.github.com>
2025-04-02 18:51:48 +0530

    image compression added

--- frontend/travel-story-app/src/utils/uploadImage.js
+++ frontend/travel-story-app/src/utils/uploadImage.js
@@ -1,22 +1,28 @@
 import apiRequest from "./apiRequest";
+import imageCompression from 'browser-image-compression';
 
 const uploadImage = async (imageFile) => {
-  const formData = new FormData();
-
-  //append image file to form data
-  formData.append('image', imageFile);
+  const options = {
+    maxSizeMB: 1, // Reduce file size to max 1MB
+    maxWidthOrHeight: 1024, // Resize dimensions
+    useWebWorker: true, // Optimize processing
+  };
 
   try {
-    const response = await apiRequest.post("/image-upload",formData,{
-      headers: {
-        'Content-Type': 'multipart/form-data', //set header for file upload
-      },
+    const compressedFile = await imageCompression(imageFile, options);
+
+    const formData = new FormData();
+    formData.append("image", compressedFile);
+
+    const response = await apiRequest.post("/image-upload", formData, {
+      headers: { "Content-Type": "multipart/form-data" },
     });
+
     return response.data;
   } catch (error) {
-    console.error('Error uploading the image:', error);
+    console.error("Error uploading the image:", error);
     throw error;
   }
-}
+};
 
 export default uploadImage;
