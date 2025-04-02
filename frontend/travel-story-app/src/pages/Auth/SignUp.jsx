a9c18798fc55a8dc3ed3323c28588af8976e7ff3
Saurbh Moynak <94280322+Saurbhmoynak@users.noreply.github.com>
2025-04-02 18:11:47 +0530

    css change

--- frontend/travel-story-app/src/pages/Auth/SignUp.jsx
+++ frontend/travel-story-app/src/pages/Auth/SignUp.jsx
@@ -4,7 +4,7 @@ import PasswordInput from "../../components/Input/PasswordInput";
 import { useNavigate } from "react-router-dom";
 import { validateEmail } from "../../utils/helper";
 import apiRequest from "../../utils/apiRequest";
-import bgImage2 from '../../assets/images/signup-image.jpg';
+import bgImage2 from "../../assets/images/signup-image.jpg";
 
 const SignUp = () => {
   const travelQuotes = [
@@ -86,7 +86,9 @@ const SignUp = () => {
       <div className="login-ui-box right-10 -top" />
       <div className="login-ui-box bg-cyan-200 -bottom-11 left-1/14" />
       <div className="container h-screen flex items-center justify-center px-20 mx-auto relative ">
-        <div style={{ backgroundImage: `url(${bgImage2})` }} className="w-2/4 h-[90vh] flex items-end bg-cover bg-center rounded-lg p-10 ">
+        <div
+          className="w-2/4 h-[90vh] flex items-end bg-cover bg-center rounded-lg p-10 "
+        >
           <div>
             <h4 className="text-5xl text-white font-bold leading-[58px]">
               Join The <br /> Adventure
@@ -97,7 +99,7 @@ const SignUp = () => {
           </div>
         </div>
 
-        <div className="w-2/5 h-[75vh] bg-white rounded-r-lg p-16 shadow-lg shadow-cyan-200 ">
+        <div style={{ backgroundImage: `url(${bgImage2})` }} className="w-2/5 h-[75vh] bg-white rounded-r-lg p-16 shadow-lg shadow-cyan-200 ">
           <form onSubmit={handleSignUp}>
             <h4 className="text-2xl font-semibold mb-7">SignUp</h4>
             <input
